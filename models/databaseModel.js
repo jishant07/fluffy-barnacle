3a86703217c125c72ff84d22eb5f5dca93b3057d
jishant07 <jishanta@gmail.com>
2022-06-29 23:53:32 -0400

    Server Test

--- models/databaseModel.js
+++ models/databaseModel.js
@@ -2,6 +2,7 @@ var dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
 const globalModel = require('../models/globalModel')
 
+console.log(process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI).catch(err =>{
     console.log("MONGO DB CONNECTION ERROR.")
 })
